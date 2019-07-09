21bef0acf43055de093063accabc6732143c8325
Kyle Albert <kyle.albert312@gmail.com>
2019-07-09 00:24:54 -0400

    Scripts + updates.

---
+++ disable-debugging.sh
@@ -0,0 +1,1 @@
+defaults write com.elgato.StreamDeck html_remote_debugging_enabled -bool NO
---
+++ enable-debugging.sh
@@ -0,0 +1,2 @@
+# Navigate to http://localhost:23654/
+defaults write com.elgato.StreamDeck html_remote_debugging_enabled -bool YES
